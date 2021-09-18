e7dd10eff3a361f3c87908cd5df7a229be205397
Hristo Panev <icancube96@gmail.com>
2021-09-18 23:22:42 +0300

    Finished mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,27 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function() {
-    if(header.classList.contains('open')) {
+
+    if(header.classList.contains('open')) { // closing the hamburger menu
+        body.classList.remove('noscroll')
         header.classList.remove('open');
-        overlay.classList.add('fade-out');
-        overlay.classList.remove('fade-in');
+        fadeElems.forEach(function(element){
+        element.classList.add('fade-out');
+        element.classList.remove('fade-in');
+        });
     }
-    else {
-        header.classList.add('open');
-        overlay.classList.add('fade-in');
-        overlay.classList.remove('fade-out');
+    else { // Opening the hamburger  menu
+        body.classList.add('noscroll')
+        header.classList.add('open'); 
+        fadeElems.forEach(function(element){
+        element.classList.add('fade-in');
+        element.classList.remove('fade-out');
+        });
+
+
     }
 });
